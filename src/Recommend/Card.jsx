ce31f1cd3724cbfc3f72f86b013ed3580f44db93
Ivan Void <npizzpak@gmail.com>
2017-08-10 17:24:03 +0300

    feat: shape product card

---
+++ src/Recommend/Card.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    return (
+      <a className="recommend-link">
+        <img className="recommend-image" src={this.props.img} alt={this.props.title} />
+        <h3 className="recommend-image-title">{this.props.title}</h3>
+        <p className="recommend-image-price">{this.props.price}</p>
+      </a>
+    )
+  }
+}
+
+export default Card;
--- src/Recommend/index.jsx
+++ src/Recommend/index.jsx
@@ -1,4 +1,28 @@
 import React, { Component } from 'react';
+import Card from './Card';
+
+const photos = [
+  {
+    'title': 'Emroided Hooded',
+    'price': '27 000 руб.',
+    'img': 'img/recommend_img_1.jpg'
+  },
+  {
+    'title': 'Relaxed Fit Stretch Jeans ',
+    'price': '22 500 руб.',
+    'img': 'img/recommend_img_2.jpg'
+  },
+  {
+    'title': 'Leather and House Check',
+    'price': '120 000 руб.',
+    'img': 'img/recommend_img_3.jpg'
+  },
+  {
+    'title': 'Leather Wingtip',
+    'price': '46 000 руб.',
+    'img': 'img/recommend_img_4.jpg'
+  },
+]
 
 class Recommend extends Component {
   render() {
@@ -7,34 +31,14 @@ class Recommend extends Component {
         <h2 className="recommend-title">WE RECOMMEND</h2>
         <div className="recommend-cards">
           <div className="row">
-            <div className="col-xs-6 col-sm-3 col-md-3">
-              <a className="recommend-link" href="#">
-                <img className="recommend-image" src="img/recommend_img_1.jpg" alt="Emroided Hooded" />
-                <h3 className="recommend-image-title">Emroided Hooded Content For Three Lines</h3>
-                <p className="recommend-image-price">27 000 руб.</p>
-              </a>
-            </div>
-            <div className="col-xs-6 col-sm-3 col-md-3">
-              <a className="recommend-link" href="#">
-                <img className="recommend-image" src="img/recommend_img_2.jpg" alt="Relaxed Fit Stretch Jeans" />
-                <h3 className="recommend-image-title">Relaxed Fit Stretch Jeans Content For Three Lines</h3>
-                <p className="recommend-image-price">22 500 руб.</p>
-              </a>
-            </div>
-            <div className="col-xs-6 col-sm-3 col-md-3">
-              <a className="recommend-link" href="#">
-                <img className="recommend-image" src="img/recommend_img_3.jpg" alt="Leather and House Check" />
-                <h3 className="recommend-image-title">Leather and House Check Content For Three Lines</h3>
-                <p className="recommend-image-price">120 000 руб.</p>
-              </a>
-            </div>
-            <div className="col-xs-6 col-sm-3 col-md-3">
-              <a className="recommend-link" href="#">
-                <img className="recommend-image" src="img/recommend_img_4.jpg" alt="Leather Wingtip Check" />
-                <h3 className="recommend-image-title">Leather Wingtip Check Content For Three Lines</h3>
-                <p className="recommend-image-price">46 000 руб.</p>
-              </a>
-            </div>
+            { photos.map((photo,num)=>{
+              console.warn(photo.title)
+              return (
+                <div className="col-xs-6 col-sm-3 col-md-3" key={num}>
+                  <Card title={photo.title} price={photo.price} img={photo.img}/>
+                </div>
+              )}
+            )}
           </div>
         </div>
       </div>
