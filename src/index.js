177bc467efd09ff20b765106b44a2a926b245ab9
Ivan Void <npizzpak@gmail.com>
2017-08-11 17:00:34 +0300

    feat: add react-intl

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import 'normalize.css';
-import 'flexboxgrid2';
-import './style.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import "normalize.css";
+import "flexboxgrid2";
+import "./style.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { IntlProvider, addLocaleData } from "react-intl";
+import en from "react-intl/locale-data/en";
+import ru from "react-intl/locale-data/ru";
+
+addLocaleData([...en, ...ru]);
+const language =
+  (navigator.languages && navigator.languages[0]) ||
+  navigator.language ||
+  navigator.userLanguage;
+
+const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
+ReactDOM.render(
+  <IntlProvider locale={language}>
+    <App />
+  </IntlProvider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
