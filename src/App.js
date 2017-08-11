82cc9a194a2dc36ceb4eb9ed09fe830c59e7acd3
Ivan Void <npizzpak@gmail.com>
2017-08-11 14:54:26 +0300

    fix: remove App class

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import {Helmet} from "react-helmet";
-import Header from './Header/';
-import Product from './Product/';
-import Recommend from './Recommend/';
-import More from './More/';
-import Footer from './Footer/';
+import React, { Component } from "react";
+import { Helmet } from "react-helmet";
+import Header from "./Header/";
+import Product from "./Product/";
+import Recommend from "./Recommend/";
+import More from "./More/";
+import Footer from "./Footer/";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Helmet>
-          <title>Long Cotton Gabardine Car Coat | Men - Burberry</title>
-        </Helmet>
-        <Header/>
-        <div className="container">
-          <Product/>
-          <Recommend/>
-          <More/>
-        </div>
-        <Footer/>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () =>
+  <div>
+    <Helmet>
+      <title>Long Cotton Gabardine Car Coat | Men - Burberry</title>
+    </Helmet>
+    <Header />
+    <div className="container">
+      <Product />
+      <Recommend />
+      <More />
+    </div>
+    <Footer />
+  </div>;
